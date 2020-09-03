17bf9a7a1b502ea152e3d44f38701332d241b0df
rsascha <sascha.rose@gmail.com>
2020-09-03 19:31:08 +0200

    Add swagger and set-token route

--- development-tools/test-service/src/app.controller.ts
+++ development-tools/test-service/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger: Logger;
+
+  constructor(private readonly appService: AppService) {
+    this.logger = new Logger("AppController")
+  }
 
   @Get()
   getHello(): string {
+    this.logger.log("The API root was called!");
     return this.appService.getHello();
+  }
+
+  @Post("/set-token")
+  setToken(): boolean {
+    this.logger.log("set-token was called!");
+    return this.appService.setToken();
   }
 }
--- development-tools/test-service/src/main.ts
+++ development-tools/test-service/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { env } from 'process';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger("bootstrap");
+  const port = env["port"] || 3000;
+
+  const options = new DocumentBuilder()
+    .setTitle('Test Service')
+    .setDescription('Just the API of the test service for nest-keycloak-connect.')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  logger.log(`Application is running on: http://localhost:${port}`);
+  logger.log(`Swagger is running on: http://localhost:${port}/swagger`);
+
+  await app.listen(port);
 }
 bootstrap();
